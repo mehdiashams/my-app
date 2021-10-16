cd7c542fb1829aa964f81160e5b4ae64b4651a25
mehdiashams <mehdia.shams@gmail.com>
2021-10-16 16:09:30 -0500

    add NavBar

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from '../assets/Search Genie.png';
+import { SearchBar } from "../SearchBar/SearchBar";
+import styles from './NavBar.module.css';
+export function NavBar() {
+    return(
+        <div className={styles['nav-bar']}>
+            <img src={logo} className={styles.logo} alt='Search Genie logo'/>
+            <SearchBar small/>
+            <button className={`button ${styles['nav-button']}`}>Sign In</button>
+            <button className={`button ${styles['nav-button']}`}>Register</button>
+        </div>
+    );
+}
